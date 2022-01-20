7b86e8538c16701e89b736b83fd8ac249d9d7240
tbluethner <95984567+tbluethner@users.noreply.github.com>
2022-01-20 22:17:42 +0100

    Update perform_http_request.h

--- sprint_endpoint/main/perform_http_request.h
+++ sprint_endpoint/main/perform_http_request.h
@@ -3,8 +3,8 @@
 #include "esp_log.h"
 #include "esp_http_client.h"
 
-#define MASTER_URL			"http://192.168.5.1/"
-#define HTTP_REQ_TAG		"PERF_HTTP_REQ"
+#define MASTER_URL      "http://192.168.5.1/"
+#define HTTP_REQ_TAG    "PERF_HTTP_REQ"
 
 esp_err_t _http_event_handler(esp_http_client_event_t *evt);
 esp_err_t perform_http_request(int method, char * signal, char * post_field, char headers[][2][50], int header_count, char * response_buf, int response_buf_len);
