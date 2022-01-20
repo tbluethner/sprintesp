f04b0bb5018316eaaed54367046069606cb37766
tbluethner <95984567+tbluethner@users.noreply.github.com>
2022-01-20 22:30:30 +0100

    sprint_endpoint file and code reorganization

--- sprint_endpoint/main/perform_http_request.h
+++ sprint_endpoint/main/perform_http_request.h
@@ -3,7 +3,7 @@
 #include "esp_log.h"
 #include "esp_http_client.h"
 
-#define MASTER_URL      "http://192.168.5.1/"
+#define MASTER_URL      "http://192.168.5.1/" //IP address of measurement device at the start of a running track
 #define HTTP_REQ_TAG    "PERF_HTTP_REQ"
 
 esp_err_t _http_event_handler(esp_http_client_event_t *evt);
