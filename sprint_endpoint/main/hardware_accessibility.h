4bb1d3dad0d27cf6290d4bb9fb23931bd5590723
tbluethner <95984567+tbluethner@users.noreply.github.com>
2022-01-20 22:13:18 +0100

    Update hardware_accessibility.h

--- sprint_endpoint/main/hardware_accessibility.h
+++ sprint_endpoint/main/hardware_accessibility.h
@@ -13,35 +13,35 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
-#define GPIO_INIT_TAG		"GPIO_INIT"
-#define SD_INIT_TAG			"SD_INIT"
-#define NETIF_AP_INIT_TAG	"NETIF_AP_INIT"
+#define GPIO_INIT_TAG		    "GPIO_INIT"
+#define SD_INIT_TAG			    "SD_INIT"
+#define NETIF_AP_INIT_TAG	  "NETIF_AP_INIT"
 #define NETIF_STA_INIT_TAG	"NETIF_STA_INIT"
-#define WIFI_INIT_TAG		"WIFI_INIT"
-#define TIMER_INIT_TAG		"TIMER_INIT"
-#define ADC_INIT_TAG		"ADC_INIT"
+#define WIFI_INIT_TAG		    "WIFI_INIT"
+#define TIMER_INIT_TAG		  "TIMER_INIT"
+#define ADC_INIT_TAG		    "ADC_INIT"
 
-#define LASER_PIN			GPIO_NUM_33	//LASER																						TOGGLE
-#define RED_LED_PIN			GPIO_NUM_27 //LASER				indicator: ON if LASER detected, OFF if LASER not detected				TOGGLE
+#define LASER_PIN			  GPIO_NUM_33	  //LASER                                                                                   TOGGLE
+#define RED_LED_PIN			GPIO_NUM_27 //LASER indicator: ON if LASER detected, OFF if LASER not detected				                TOGGLE
 
 #define BUZZER_PIN			GPIO_NUM_32	//Audio (Buzzer)	indicator: various signals (Eg. synchronization, start of measurement)	PWM
-#define BLUE_LED_PIN		GPIO_NUM_25	//WIFI connection	indicator: ON if connected, BLINKING if disconnected					PWM
-#define GREEN_LED_PIN		GPIO_NUM_26	//synchronization	indicator: OFF if no sync attempted, blinking if synced					PWM
+#define BLUE_LED_PIN		GPIO_NUM_25	//WIFI connection	indicator: ON if connected, BLINKING if disconnected					          PWM
+#define GREEN_LED_PIN		GPIO_NUM_26	//synchronization	indicator: OFF if no sync attempted, blinking if synced					        PWM
 
-#define BUZZER_TIMER		LEDC_TIMER_0
+#define BUZZER_TIMER		  LEDC_TIMER_0
 #define BLUE_LED_TIMER		LEDC_TIMER_1
 #define GREEN_LED_TIMER		LEDC_TIMER_2
 
 #define BUZZER_CHAN			LEDC_CHANNEL_0
 #define BLUE_LED_CHAN		LEDC_CHANNEL_1
-#define GREEN_LED_CHAN		LEDC_CHANNEL_2
+#define GREEN_LED_CHAN	LEDC_CHANNEL_2
 
 #define BUZZER_DUTY_RES		LEDC_TIMER_1_BIT
-#define LED_DUTY_RES		LEDC_TIMER_20_BIT
+#define LED_DUTY_RES		  LEDC_TIMER_20_BIT
 
 #define LED_DUTY_MAX		1048576				//2^20 == 2^LEDC_TIMER_20_BIT
 #define BLUE_LED_DUTY		(LED_DUTY_MAX / 2)
-#define GREEN_LED_DUTY		(LED_DUTY_MAX / 8)
+#define GREEN_LED_DUTY	(LED_DUTY_MAX / 8)
 
 #define BUZZER_NORM_FREQ	4000
 #define BLUE_LED_FREQ		2
