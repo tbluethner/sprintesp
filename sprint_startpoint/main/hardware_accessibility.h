327ac37225d6cda79c9aa55fb7beba511e198eb2
tbluethner <95984567+tbluethner@users.noreply.github.com>
2022-01-31 00:22:10 +0100

    sprint_startpoint file and code reorganization and structurization

--- sprint_startpoint/main/hardware_accessibility.h
+++ sprint_startpoint/main/hardware_accessibility.h
@@ -10,44 +10,44 @@
 #include "lwip/sys.h"
 #include "esp_log.h"
 
-#define GPIO_INIT_TAG		"GPIO_INIT"
-#define NETIF_AP_INIT_TAG	"NETIF_AP_INIT"
-#define WIFI_INIT_TAG		"WIFI_INIT"
-#define TIMER_INIT_TAG		"TIMER_INIT"
-#define ADC_INIT_TAG		"ADC_INIT"
+#define GPIO_INIT_TAG       "GPIO_INIT"
+#define NETIF_AP_INIT_TAG   "NETIF_AP_INIT"
+#define WIFI_INIT_TAG       "WIFI_INIT"
+#define TIMER_INIT_TAG      "TIMER_INIT"
+#define ADC_INIT_TAG        "ADC_INIT"
 
-#define LASER_PIN			GPIO_NUM_33	//LASER																						TOGGLE
-#define RED_LED_PIN			GPIO_NUM_27 //LASER				indicator: ON if LASER detected, OFF if LASER not detected				TOGGLE
+#define LASER_PIN           GPIO_NUM_33	//LASER																						TOGGLE
+#define RED_LED_PIN         GPIO_NUM_27 //LASER				indicator: ON if LASER detected, OFF if LASER not detected				TOGGLE
 
-#define BUZZER_PIN			GPIO_NUM_32	//Audio (Buzzer)	indicator: various signals (Eg. synchronization, start of measurement)	PWM
-#define BLUE_LED_PIN		GPIO_NUM_25	//WIFI connection	indicator: ON if connected, BLINKING if disconnected					PWM
-#define GREEN_LED_PIN		GPIO_NUM_26	//synchronization	indicator: OFF if no sync attempted, blinking if synced					PWM
+#define BUZZER_PIN          GPIO_NUM_32	//Audio (Buzzer)	indicator: various signals (Eg. synchronization, start of measurement)	PWM
+#define BLUE_LED_PIN        GPIO_NUM_25	//WIFI connection	indicator: ON if connected, BLINKING if disconnected					PWM
+#define GREEN_LED_PIN       GPIO_NUM_26	//synchronization	indicator: OFF if no sync attempted, blinking if synced					PWM
 
-#define BUZZER_TIMER		LEDC_TIMER_0
-#define BLUE_LED_TIMER		LEDC_TIMER_1
-#define GREEN_LED_TIMER		LEDC_TIMER_2
+#define BUZZER_TIMER        LEDC_TIMER_0
+#define BLUE_LED_TIMER      LEDC_TIMER_1
+#define GREEN_LED_TIMER     LEDC_TIMER_2
 
-#define BUZZER_CHAN			LEDC_CHANNEL_0
-#define BLUE_LED_CHAN		LEDC_CHANNEL_1
-#define GREEN_LED_CHAN		LEDC_CHANNEL_2
+#define BUZZER_CHAN         LEDC_CHANNEL_0
+#define BLUE_LED_CHAN       LEDC_CHANNEL_1
+#define GREEN_LED_CHAN      LEDC_CHANNEL_2
 
-#define BUZZER_DUTY_RES		LEDC_TIMER_1_BIT
-#define LED_DUTY_RES		LEDC_TIMER_20_BIT
+#define BUZZER_DUTY_RES     LEDC_TIMER_1_BIT
+#define LED_DUTY_RES        LEDC_TIMER_20_BIT
 
-#define LED_DUTY_MAX		1048576				//2^20 == 2^LEDC_TIMER_20_BIT
-#define BLUE_LED_DUTY		(LED_DUTY_MAX / 2)
-#define GREEN_LED_DUTY		(LED_DUTY_MAX / 8)
+#define LED_DUTY_MAX        1048576				//2^20 == 2^LEDC_TIMER_20_BIT
+#define BLUE_LED_DUTY       (LED_DUTY_MAX / 2)
+#define GREEN_LED_DUTY      (LED_DUTY_MAX / 8)
 
-#define BUZZER_NORM_FREQ	4000
-#define BLUE_LED_FREQ		2
-#define GREEN_LED_FREQ		1
+#define BUZZER_NORM_FREQ    4000
+#define BLUE_LED_FREQ       2
+#define GREEN_LED_FREQ      1
 
-#define TIMER_DIVIDER		40000  //  Hardware timer clock divider
-#define TIMER_SCALE			(TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
-#define TIMER_COUNTSTART	0
+#define TIMER_DIVIDER       40000  //  Hardware timer clock divider
+#define TIMER_SCALE         (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
+#define TIMER_COUNTSTART    0
 
 
-#define ADC_CHANNEL			ADC1_CHANNEL_6
+#define ADC_CHANNEL         ADC1_CHANNEL_6
 
 typedef struct custom_esp_netif_inherent_config {
     esp_netif_flags_t flags;         /*!< flags that define esp-netif behavior */
